767898b8b0e6c4565febec61be5bc3efe4fedddf
Defianon2207 <109776668+Defianon2207@users.noreply.github.com>
2026-07-14 18:05:16 +0530

    Added more of functool

--- Functool.py
+++ Functool.py
@@ -1,4 +1,4 @@
-from functools import cache
+from functools import *
 
 @cache
 def fibonacci(n):
@@ -10,3 +10,21 @@ def fibonacci(n):
     return fibonacci(n - 1) + fibonacci(n - 2)
 
 print(fibonacci(3))
+
+class DataSet:
+
+    def __init__(self, sequence_of_numbers):
+        self._data = tuple(sequence_of_numbers)
+
+    @cached_property
+    def stdev(self):
+        return statistics.stdev(self._data)
+n = [12,34,23,43,52,56]
+p =  DataSet(n)
+print(p._data)
+
+def power(base, exponent):
+    return base ** exponent
+
+square = partial(power, exponent =2)
+print(square(5))
