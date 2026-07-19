cd6060c6a3d862c319df1827a645ef988780dbcd
Defianon2207 <109776668+Defianon2207@users.noreply.github.com>
2026-07-19 20:57:04 +0530

    Added more fucntion to OsPath

--- FileSystem/OsPathExample.py
+++ FileSystem/OsPathExample.py
@@ -2,3 +2,25 @@ import os
 
 print(os.getcwd())
 print(os.path.abspath("FileSystem.txt"))
+
+paths = [
+    "/Users/rahul/project/src/main.py",
+    "/Users/rahul/project/src/utils.py",
+    "/Users/rahul/project/tests/test.py"
+]
+
+print(os.path.commonpath(paths))
+
+# Example of common path
+paths = [
+    "apple.txt",
+    "application.py",
+    "apply.doc"
+]
+
+print(os.path.commonprefix(paths))
+
+# Example of dirname 
+path = "/home/rahul/Documents/report.pdf"
+
+print(os.path.dirname(path))
