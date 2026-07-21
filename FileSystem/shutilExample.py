f9d5157055a90da80e60f37aec6d3ec32563e645
Defianon2207 <109776668+Defianon2207@users.noreply.github.com>
2026-07-21 11:38:17 +0530

    Completed Shutil Examples

--- FileSystem/shutilExample.py
+++ FileSystem/shutilExample.py
@@ -76,3 +76,63 @@ print(time.ctime(destination_info.st_mtime))
 
 
 #Example of shutil.copy
+
+result = shutil.copy("first.txt","folder_a")
+print(result)
+
+# shutil.copy2(src, dst, *, follow_symlinks=True)
+# Identical to copy() except that copy2() also attempts to preserve file metadata.
+
+#shutil.ignore_patterns
+# This factory function creates a function that can be used as a callable for copytree()'s ignore argument, ignoring files and directories that match one of the glob-style patterns provided. See the example below.
+# Example 
+# shutil.ignore_patterns(
+#         "*.log",
+#         "*.pyc",
+#         "__pycache__"
+#     )
+
+
+# Copy Tree example 
+
+#Commenting this so no new folders are created 
+# result = shutil.copytree("folder_a", "FileSysytem_backup")
+# shutil.rmtree("FileSysytem_backup")
+
+# shutil.move(src, dst, copy_function=copy2)
+
+usage = shutil.disk_usage("folder_a")
+print("Usage -> ", usage)
+
+# shutil.chown(path, user=None, group=None, *, dir_fd=None, follow_symlinks=True)
+# Change owner user and/or group of the given path.
+
+python_path = shutil.which("python3")
+
+print(python_path)
+
+ffmpeg = shutil.which("ffmpeg")
+print(ffmpeg)
+
+#By default, which() searches directories listed in the system’s PATH environment variable.
+
+#You can provide custom path to it 
+# result = shutil.which(
+#     "python3",
+#     path="/usr/bin:/opt/homebrew/bin"
+# )
+
+
+#Archival operations
+# shutil.make_archive("backup", "zip", root_dir="folder_a")
+
+print(shutil.get_archive_formats())
+print(shutil.get_unpack_formats())
+
+# shutil.register_archive_format
+# shutil.unregister_archive_format(name)
+# shutil.unpack_archive(filename[, extract_dir[, format[, filter]]])
+
+# shutil.register_unpack_format(name, extensions, function[, extra_args[, description]])
+# shutil.unregister_unpack_format(name)
+
