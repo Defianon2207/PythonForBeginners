25a40d1e42014171d6f00f71d2d9a241d36c7431
Defianon2207 <109776668+Defianon2207@users.noreply.github.com>
2026-07-27 15:13:49 +0530

    More example of Local

--- ConcurrentExecution/ThreadingPart2.py
+++ ConcurrentExecution/ThreadingPart2.py
@@ -17,4 +17,35 @@ class MyLocal(local):
 
 
 mydata = MyLocal(color="red")
-print(mydata.squared())
+# print(mydata.squared())
+# print(mydata.color)
+# print(mydata.number, mydata.name)
+# print(mydata.__dict__)
+
+# creating multiple threads
+
+log =[]
+
+def worker(color):
+    mydata.color = color
+    mydata.number = 11
+
+    time.sleep(1)
+
+    print(
+        threading.current_thread().name,
+        mydata.color,
+        mydata.__dict__
+    )
+
+threads = [threading.Thread(target=worker,args =('red',), name="Worker1"),
+threading.Thread(target=worker,args =('green',), name="Worker2"),
+]
+
+for thread in threads:
+    thread.start()
+
+for thread in threads:
+      thread.join()
+
+print("Log",log)
