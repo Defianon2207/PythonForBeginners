76141c64c6588097f3591b6ae4aa23de537b08a8
Defianon2207 <109776668+Defianon2207@users.noreply.github.com>
2026-07-28 09:12:18 +0530

    Handling Daemon Thread

--- ConcurrentExecution/SubClassingThread.py
+++ ConcurrentExecution/SubClassingThread.py
@@ -55,5 +55,50 @@ class SubclassExampleOfThread(threading.Thread):
 
 thread = SubclassExampleOfThread("Hey Siri")
 thread.start()
+print(thread.is_alive(), threading.current_thread())
 thread.join()
 
+
+#Custom exceptHook()
+
+def handle_except_hook(args):
+    print(f"Custom Exception, {args.thread.name}, failed : {args.exc_value}")
+
+threading.excepthook = handle_except_hook
+
+def worker():
+    raise ValueError("Invalid Payment ammount")
+
+thread = threading.Thread(
+    target = worker,
+    name ="Except_worker"
+)
+
+thread.start()
+thread.join()
+
+#Daemon Thread
+
+stop_event = threading.Event()
+
+def background_worker():
+    while not stop_event.is_set():
+        print("Background work in progress")
+        time.sleep(1)
+    print("Exiting gracefully")
+
+thread = threading.Thread(
+    target = background_worker,
+    name = "backgorund_worker",
+    daemon = False
+)
+
+thread.start()
+time.sleep(3)
+
+print("Requesting shutdown")
+stop_event.set()
+
+thread.join()
+
+print("Program finished")
