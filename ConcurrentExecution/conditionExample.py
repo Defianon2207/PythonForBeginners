ca0e709606bd84fa353c831d83009c6a8ca9522f
Defianon2207 <109776668+Defianon2207@users.noreply.github.com>
2026-08-04 12:49:44 +0530

    More of condition

--- ConcurrentExecution/conditionExample.py
+++ ConcurrentExecution/conditionExample.py
@@ -37,3 +37,50 @@ producer_thread.start()
 
 consumer_thread.join()
 producer_thread.join()
+
+## Practice
+# Create two threads:
+
+# Producer: Adds numbers 1–5 to a shared list, one at a time.
+# Consumer: Removes and prints numbers from the list.
+
+# Rules:
+
+# The consumer must wait using condition.wait() whenever the list is empty.
+# The producer must call condition.notify() after adding an item.
+# Both threads must access the shared list inside with condition:.
+# Stop the consumer after it consumes all five numbers.
+
+N_list = []
+
+def n_consumer():
+    print("Consumer triggered")
+    for _ in range(5):
+        with condition:
+            while not N_list:
+                condition.wait()
+            number =  N_list.pop()
+            print(f"Consumed: {number} ")
+           
+
+def n_producer():
+        k =1
+        while k < 6:
+            time.sleep(1)
+            with condition:
+                N_list.append(k)
+                print(f"Produced: {k}")
+                condition.notify()
+            k = k+1
+
+            
+thread1 = threading.Thread(target=n_producer)
+thread2 = threading.Thread(target=n_consumer)
+
+thread2.start()
+thread1.start()
+thread1.join()
+thread2.join()
+
+
+
