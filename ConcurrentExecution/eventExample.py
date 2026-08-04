eb8e03b8ce0ac4a9798317de7a7eece864c2fa25
Defianon2207 <109776668+Defianon2207@users.noreply.github.com>
2026-08-04 18:40:17 +0530

    event example

---
+++ ConcurrentExecution/eventExample.py
@@ -0,0 +1,37 @@
+import threading
+import time
+
+server_ready = threading.Event()
+
+
+def worker(name):
+    print(f"{name}: Waiting for the server...")
+
+    server_ready.wait()
+
+    print(f"{name}: Server is ready. Starting work.")
+
+
+def start_server():
+    print("Server: Starting...", server_ready.is_set())
+    time.sleep(3)
+
+    server_ready.set()
+    print("Server: Ready!",server_ready.is_set())
+
+
+threads = [
+    threading.Thread(target=worker, args=(f"Worker {i}",))
+    for i in range(1, 4)
+]
+
+for thread in threads:
+    thread.start()
+
+server_thread = threading.Thread(target=start_server)
+server_thread.start()
+
+for thread in threads:
+    thread.join()
+
+server_thread.join()
