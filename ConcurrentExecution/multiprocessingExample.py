96cebcd6f05551e9755093c1733d03ea4aa2a02e
Defianon2207 <109776668+Defianon2207@users.noreply.github.com>
2026-08-06 09:16:17 +0530

    MPP example

--- ConcurrentExecution/multiprocessingExample.py
+++ ConcurrentExecution/multiprocessingExample.py
@@ -1,8 +1,16 @@
-from multiprocessing import Pool
+from multiprocessing import Pool, Process
+
 
 def f(x):
     return x*x
 
+def fpro(name):
+    print(f"Name is : {name}")
+
 if __name__ == '__main__':
     with Pool(5) as p:
         print(p.map(f, [1, 2, 3]))
+
+    p = Process(target =fpro, args=("Defianon",))
+    p.start()
+    p.join()
