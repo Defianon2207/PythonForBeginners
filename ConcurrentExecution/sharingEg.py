080c21ae879f6c6a8641de3c5944ebb0ab63229f
Defianon2207 <109776668+Defianon2207@users.noreply.github.com>
2026-08-25 11:28:02 +0530

    shared value example

--- ConcurrentExecution/sharingEg.py
+++ ConcurrentExecution/sharingEg.py
@@ -5,6 +5,11 @@ def f(n, a):
     for i in range(len(a)):
         a[i] = -a[i]
 
+def increment(counter):
+    for _ in range(100_000):
+        with counter.get_lock():
+            counter.value += 1
+
 if __name__ == '__main__':
     num = Value('d', 0.0)
     arr = Array('i', range(10))
@@ -15,3 +20,18 @@ if __name__ == '__main__':
 
     print(num.value)
     print(arr[:])
+    counter = Value("i", 0)
+
+    processes = [
+        Process(target=increment, args=(counter,))
+        for _ in range(4)
+    ]
+
+    for process in processes:
+        process.start()
+
+    for process in processes:
+        process.join()
+
+    print(counter.value)
+
