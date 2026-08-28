4cd519bc02d86a17703575c6a8d4bc3cab4ecf99
Defianon2207 <109776668+Defianon2207@users.noreply.github.com>
2026-08-28 20:53:59 +0530

    proxy dict

--- ConcurrentExecution/manager2.py
+++ ConcurrentExecution/manager2.py
@@ -4,6 +4,9 @@ from multiprocessing import Process, Manager
 def add_user(users, user_id, name):
     users[user_id] = name
 
+def calculate_square(number, results):
+    results.append(number ** 2)
+
 
 if __name__ == "__main__":
     with Manager() as manager:
@@ -22,3 +25,21 @@ if __name__ == "__main__":
             process.join()
 
         print(dict(users))
+    with Manager() as manager:
+        results = manager.list()
+
+        processes = [
+            Process(
+                target=calculate_square,
+                args=(number, results)
+            )
+            for number in range(1, 6)
+        ]
+
+        for process in processes:
+            process.start()
+
+        for process in processes:
+            process.join()
+
+        print(list(results))
