456b550097a775833bd8804cb929850af0abb868
Defianon2207 <109776668+Defianon2207@users.noreply.github.com>
2026-09-03 12:41:51 +0530

    map examples

--- ConcurrentExecution/concurrentFutures.py
+++ ConcurrentExecution/concurrentFutures.py
@@ -40,3 +40,53 @@ with ThreadPoolExecutor(max_workers=1) as executor:
 
     result = future.result()
     print("Result:", result)
+
+
+# Map function example
+def square(a):
+    return a**2
+
+with ThreadPoolExecutor(max_workers=4) as executor:
+    future = executor.map(square,[2,3,4,5,6])
+
+    print("square result",list(future))
+
+
+
+
+#     5. Useful Future methods
+# future.done()
+
+# Checks whether the task has finished:
+
+# print(future.done())
+
+# It returns True or False.
+
+# future.running()
+
+# Checks whether the task is currently running:
+
+# print(future.running())
+# future.cancel()
+
+# Attempts to cancel the task:
+
+# cancelled = future.cancel()
+
+# Cancellation succeeds only if the executor has not started running the task.
+
+# future.cancelled()
+
+# Checks whether it was successfully cancelled:
+
+# print(future.cancelled())
+# future.exception()
+
+# Returns the exception raised by the task:
+
+# error = future.exception()
+
+# It returns None if the task completed successfully.
+
+# 6. Handling exceptions from submit()
