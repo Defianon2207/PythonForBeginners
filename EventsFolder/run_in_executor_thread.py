77dd431f9e7e12d51f5059ab33a15d4798593a79
Defianon2207 <109776668+Defianon2207@users.noreply.github.com>
2026-09-19 10:31:35 +0530

    example of thread running  even when main is closed

---
+++ EventsFolder/run_in_executor_thread.py
@@ -0,0 +1,32 @@
+import asyncio
+import time
+
+
+def save_large_file():
+    print("Saving started")
+    time.sleep(3)
+    print("Saving finished")
+
+
+async def main():
+    loop = asyncio.get_running_loop()
+
+    # Submit the function, but don't await its result.
+    loop.run_in_executor(None, save_large_file)
+
+    print("main() finished")
+
+
+loop = asyncio.new_event_loop()
+
+try:
+    loop.run_until_complete(main())
+finally:
+    print("Waiting for executor threads...")
+
+    loop.run_until_complete(
+        loop.shutdown_default_executor()
+    )
+
+    print("Executor shut down")
+    loop.close()
